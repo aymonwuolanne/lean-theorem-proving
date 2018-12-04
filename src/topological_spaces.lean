ab2efcf2d759bc86fd962af38f445c9484101407
Aymon <u6402679@anu.edu.au>
2018-12-04 12:47:48 +1100

    instantiated nat as a top space

--- src/topological_spaces.lean
+++ src/topological_spaces.lean
@@ -7,27 +7,34 @@ import analysis.real
 
 noncomputable theory
 
-example : topological_space ℝ := by apply_instance
-
 def I := { x : ℝ | 0 ≤ x ∧ x ≤ 1 }
  
 def foo : topological_space I := by apply_instance
-
-structure path (X : Type) [topological_space X] :=
-(γ : I → X)
-(w : continuous γ)
-
-def path' (X : Type) [topological_space X] := continuous_map I X
 
 open category_theory.examples
 
 def 𝕀 : Top := { α := I }
 
-def path'' (X : Top) := 𝕀 ⟶ X
+def path (X : Top) := 𝕀 ⟶ X
 
-#check path''
-
-#print subtype.topological_space
-#print foo
+#check path
 
 #print I
+
+
+-- example instantiating ℕ with the discrete topology from basics
+open set
+
+-- define every subset of ℕ to be open
+def nat_isopen : set ℕ → Prop := (λ (S : set ℕ), true)
+-- prove the relevant properties of a topological space 
+def nat_isopen_univ : nat_isopen univ := trivial
+def nat_isopen_inter : ∀ s t, nat_isopen s → nat_isopen t → nat_isopen (s ∩ t) := 
+λ s t, (λ hs ht, trivial)
+def nat_isopen_sUnion : ∀ s, (∀ t ∈ s, nat_isopen t) → nat_isopen (⋃₀ s) := 
+λ s, (λ h, trivial)
+
+instance nat_top : topological_space ℕ := 
+⟨nat_isopen, nat_isopen_univ, nat_isopen_inter, nat_isopen_sUnion⟩
+
+ 
